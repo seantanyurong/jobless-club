7ca876c23367ff747423dac41e7461268ac11283
Sean Tan <sean.tan@sean.tan-GHKX6X1N9C>
2024-08-26 15:53:09 +0800

    feature: update money

--- app/page.tsx
+++ app/page.tsx
@@ -162,7 +162,7 @@ export default function Home() {
                     alt='fire'
                     className='absolute left-2 top-1/2 -translate-y-1/2'
                   />
-                  <p className='text-xs font-semibold w-[65px] text-center'>$830/mo</p>
+                  <p className='text-xs font-semibold w-[65px] text-center'>$920/mo</p>
                 </div>
                 <div className='bg-white rounded-full px-2 py-1 pl-12 relative hidden lg:block ml-auto'>
                   <Image
@@ -172,7 +172,7 @@ export default function Home() {
                     alt='fire'
                     className='absolute left-3 top-1/2 -translate-y-1/2'
                   />
-                  <p className='text-sm font-semibold w-[80px] text-center'>$830/mo</p>
+                  <p className='text-sm font-semibold w-[80px] text-center'>$920/mo</p>
                 </div>
               </div>
               <p className='text-xs lg:text-base'>
